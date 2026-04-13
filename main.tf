cbdb3414d155cf3fa1cc24858645b4851468b8ab
Divyya Sai Gorantla <123133078+DivyaGorantl@users.noreply.github.com>
2026-04-13 19:31:43 +0530

    Fix formatting of image_id in main.tf

--- main.tf
+++ main.tf
@@ -1,6 +1,6 @@
 resource "aws_launch_template" "web_server_as" {
     name = "myproject"
-    image_id           = "ami-051de6a4e7ae45f77"
+    image_id = "ami-051de6a4e7ae45f77"
     vpc_security_group_ids = [aws_security_group.web_server.id]
     instance_type = "t3.micro"
     key_name = "MYNEW"
